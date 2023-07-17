e1f6095e401f7077f4b2cc8f69bd448060955b04
Igor Dias <igorverissimo02@hotmail.com>
2023-07-17 16:55:27 -0300

    chore: added environment variables

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,10 @@ from django.db.models import Q
 from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.pagination import make_pagination
-
+import os
 from recipes.models import Recipe
 
-QTY_PAGES_PAGINATION = 1
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def home(request):
@@ -14,7 +14,7 @@ def home(request):
     ).order_by('-id')
 
     page_obj, pagination_range = make_pagination(
-        request, recipes, QTY_PAGES_PAGINATION)
+        request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -31,7 +31,7 @@ def category(request, category_id):
     )
 
     page_obj, pagination_range = make_pagination(
-        request, recipes, QTY_PAGES_PAGINATION)
+        request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -64,7 +64,7 @@ def search(request):
     ).order_by('-id')
 
     page_obj, pagination_range = make_pagination(
-        request, recipes, QTY_PAGES_PAGINATION)
+        request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
