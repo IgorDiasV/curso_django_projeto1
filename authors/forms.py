dbb46e172b4533ab528361897adad779059b0fe4
Igor Dias <igorverissimo02@hotmail.com>
2023-07-18 10:39:35 -0300

    feat: Added password confirmation field

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class RegisterForm(forms.ModelForm):
+
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -19,3 +28,21 @@ class RegisterForm(forms.ModelForm):
             'email': 'E-mail',
             'password': 'Password',
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+
+        if password != password2:
+            password_confirmation_error = ValidationError(
+                'Password and password2 must be equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                'password': password_confirmation_error,
+                'password2': [
+                    password_confirmation_error,
+                ],
+            })
