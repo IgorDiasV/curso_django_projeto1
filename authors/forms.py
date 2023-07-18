2c0ba4b70fd7f7d0f0fc137842338f4e30507d22
Igor Dias <igorverissimo02@hotmail.com>
2023-07-18 10:57:47 -0300

    feat: added strong password validation using regex

--- authors/forms.py
+++ authors/forms.py
@@ -1,9 +1,31 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
 
 
 class RegisterForm(forms.ModelForm):
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        }),
+        validators=[strong_password]
+    )
 
     password2 = forms.CharField(
         required=True,
