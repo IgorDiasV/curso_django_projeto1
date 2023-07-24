17fec390c7048844e89a56299eb0f249a9255c5b
Igor Dias <igorverissimo02@hotmail.com>
2023-07-24 18:48:37 -0300

    feat: added the option to create recipes

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -1,10 +1,65 @@
+from collections import defaultdict
+
 from django import forms
+from django.core.exceptions import ValidationError
 from recipes.models import Recipe
+from utils.django_forms import add_attr
 
 
 class AuthorRecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._my_errors = defaultdict(list)
+
+        add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
+
     class Meta:
         model = Recipe
         fields = 'title', 'description', 'preparation_time', \
             'preparation_time_unit', 'servings', 'servings_unit', \
             'preparation_steps', 'cover'
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pedaços', 'Pedaços'),
+                    ('Pessoas', 'Pessoas'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Horas', 'Horas'),
+                ),
+            ),
+        }
+
+    def clean(self, *args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        cd = self.cleaned_data
+
+        title = cd.get('title')
+        description = cd.get('description')
+
+        if title == description:
+            self._my_errors['title'].append('Cannot be equal to description')
+            self._my_errors['description'].append('Cannot be equal to title')
+
+        if self._my_errors:
+            raise ValidationError(self._my_errors)
+
+        return super_clean
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+
+        if len(title) < 5:
+            self._my_errors['title'].append('Must have at least 5 chars.')
+
+        return title
